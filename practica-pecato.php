6728540a8fd4c319dc91d5c1137fb1bd0891b119
buyiyo <84044195+buyiyo@users.noreply.github.com>
2021-05-11 22:18:23 +0200

    Subir script

---
+++ practica-pecato.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @param int parámetro de entrada uno funcion resta
+ * @param int parámetro de entrada dos funcion resta
+ * @return int parámetro de salida funcion resta
+ * @author Pedro Cabrera de la Torre
+ * @version 1
+ * 
+ */
+
+ function resta ($puno,$pdos) {
+    $queda=$puno-$dos;
+    return $queda;
+ }
+
+/**
+ * @param int parámetro de entrada uno funcion suma
+ * @param int parámetro de entrada dos funcion suma
+ * @return int parámetro de salida funcion suma
+ * @author Pedro Cabrera de la Torre
+ * @version 2
+ * 
+ */
+function suma($uno,$dos) {
+    $total=$uno+$dos;
+    return $total;
+}
